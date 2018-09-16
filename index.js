b576070e5ab129fb4bf076aa11592ab02599a776
jose edinaldo <edinaldofox@gmail.com>
2018-09-16 13:37:28 -0300

    modif function, generated wallet for method

--- index.js
+++ index.js
@@ -11,13 +11,13 @@ var bch = require('./coins/bch.js');
 var eth = require('./coins/eth.js');
 var eos = require('./coins/eos.js');
 
-async function main() {
+function main() {
   var symbol = process.argv[2];
   if (symbol)
     symbol = symbol.toLowerCase();
 
   var seed = crypto.randomBytes(32);
-  
+
   var wallet;
   if (['btc','btg','dash','dcr','doge','ltc','mona','qtum','rdd','zec'].indexOf(symbol) >= 0) {
     wallet = btc.generateWallet(seed, symbol);
@@ -60,23 +60,23 @@ async function main() {
   doc.fontSize(30);
 
   doc.text(
-    `${symbol.toUpperCase()} Paper Wallet`, 
+    `${symbol.toUpperCase()} Paper Wallet`,
     0, 50,
     {align: 'center'});
-  
+
   doc.fontSize(20);
-  
+
   doc.text('Public Receiving Address', 100, 110);
   doc.fontSize(12);
   doc.text(wallet.publicAddress, 100, 135);
   doc.image(publicAddressQRFile, 100, 160);
-  
+
   doc.fontSize(20);
   doc.text('Private Spending Key', 100, 440);
   doc.fontSize(12);
   doc.text(wallet.privateKey, 100, 465);
   doc.image(privateKeyQRFile, 100, 490);
-  
+
   doc.end();
 
   var addressQRText = qrText.generate(wallet.publicAddress);
@@ -84,7 +84,7 @@ async function main() {
 
   console.log(`${symbol.toUpperCase()} Paper Wallet`);
   console.log();
-  
+
   console.log('Public Receiving Address');
   console.log(wallet.publicAddress);
   console.log(addressQRText);
@@ -94,8 +94,47 @@ async function main() {
   console.log(wallet.privateKey);
   console.log(privateKeyQRText);
   console.log();
-  
+
   console.log(`Paper wallet file created: ${pdfFile}`);
 }
 
-main();
+exports.wallet =  function(symbol) {
+    if (symbol)
+        symbol = symbol.toLowerCase();
+
+    var seed = crypto.randomBytes(32);
+
+    var wallet;
+    if (['btc','btg','dash','dcr','doge','ltc','mona','qtum','rdd','zec'].indexOf(symbol) >= 0) {
+        wallet = btc.generateWallet(seed, symbol);
+    } else if (['bch'].indexOf(symbol) >= 0) {
+        wallet = bch.generateWallet(seed, symbol);
+    } else if (['etc','eth'].indexOf(symbol) >= 0) {
+        wallet = eth.generateWallet(seed, symbol);
+    } else if (['eos'].indexOf(symbol) >= 0) {
+        wallet = eos.generateWallet(seed, symbol);
+    } else {
+        console.log('Usage: paper-wallet [btc|bch|btg|dash|dcr|doge|eos|eth|etc|ltc|mona|qtum|rdd|zec]');
+        return;
+    }
+
+    var publicAddressQR = qrImg.imageSync(wallet.publicAddress, {
+        size: 8,
+        margin: 0
+    });
+
+    var privateKeyQR = qrImg.imageSync(wallet.privateKey, {
+        size: 8,
+        margin: 0
+    });
+
+    temp.track();
+
+    var publicAddressQRFile = temp.path();
+    fs.writeFileSync(publicAddressQRFile, publicAddressQR);
+    var privateKeyQRFile = temp.path();
+    fs.writeFileSync(privateKeyQRFile, privateKeyQR);
+
+
+    return wallet;
+}
